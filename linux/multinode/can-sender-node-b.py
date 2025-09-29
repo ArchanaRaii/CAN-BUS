f6283cfdd88eed761afc712a90075e7594144a35
Archana Rai <112776047+ArchanaRaii@users.noreply.github.com>
2025-09-29 14:26:37 +0530

    NODE B (SENDER)

---
+++ linux/multinode/can-sender-node-b.py
@@ -0,0 +1,23 @@
+import can
+import time
+import random
+
+def main():
+    bus = can.interface.Bus(channel="vcan0", interface="socketcan")
+    start_time = time.time()
+    arbitration_ID = 0xAB
+    role = "NODE B"
+    
+    try:
+        while True:
+            random_data = [random.randint(0,255) for _ in range(8)]
+            msg = can.Message(arbitration_id=arbitration_ID, data = random_data, is_extended_id=False)
+            msg.timestamp = time.time() - start_time
+            bus.send(msg)
+            print(f"{role} 0x{msg.arbitration_id:X} sent data packet....")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        bus.shutdown()
+
+if __name__ == "__main__":
+    main()
